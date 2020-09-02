1dd37855b4de2db58144b66951d406df9b245c15
James Su-Shum <sushumjames@gmail.com>
2020-09-02 13:43:15 -0400

    Initialized exercise, created Vehicle base class and subclasses, used virtual and override on Drive method

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace gary_garage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zero fx = new Zero();
+            Cessna oneSevenTwo = new Cessna();
+            Tesla modelS = new Tesla();
+            Ram fifteenHundred = new Ram();
+
+            fx.Drive();
+            oneSevenTwo.Drive();
+            modelS.Drive();
+            fifteenHundred.Drive();
+        }
+    }
+
+    public class Zero : Vehicle
+    {
+        public double BatteryKwh { get; set; }
+
+        public void ChargeBattery()
+        {
+            Console.WriteLine("Battery low...please charge me!");
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine("Pshhhhhh...");
+        }
+    }
+
+    public class Cessna : Vehicle
+    {
+        public double FuelCapacity { get; set; }
+
+        public void RefuelTank()
+        {
+            Console.WriteLine("Fuel low...I need gas!");
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine("Whoooshhh...");
+        }
+    }
+
+    public class Tesla : Vehicle
+    {
+        public double BatteryKwh { get; set; }
+
+        public void ChargeBattery()
+        {
+            Console.WriteLine("Battery low...take me to a supercharger now!");
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine("Cricket...cricket...");
+        }
+    }
+
+    public class Ram : Vehicle
+    {
+        public double FuelCapacity { get; set; }
+
+        public void RefuelTank()
+        {
+            Console.WriteLine("Low fuel...just dispose of me!");
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine("Cough...cough...global warming");
+        }
+    }
+
+}
---
+++ Vehicle.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace gary_garage
+{
+  public class Vehicle
+  {
+    public string MainColor { get; set; }
+    public string MaximumOccupancy { get; set; }
+
+    public virtual void Drive()
+    {
+      Console.WriteLine("Vrooom!");
+    }
+  }
+
+}
